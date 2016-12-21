da3fdd2d43c2ced8d2e269ba9ba58565019f26e8
JT Cho <jonathan.t.cho@gmail.com>
2016-12-21 04:30:26 -0800

    More resources

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,14 @@ var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var sass = require('gulp-ruby-sass');
+var livereload = require('gulp-livereload');
+var spawn = require('child_process').spawn;
+var node;
 
 gulp.task('sass', function() {
   return sass('./src/assets/styles/**/*.scss')
-    .pipe(gulp.dest('./dist/assets/styles'));
+    .pipe(gulp.dest('./dist/assets/styles'))
+    .pipe(livereload());
 });
 
 gulp.task('js', function() {
@@ -25,4 +29,40 @@ gulp.task('html', function() {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['sass', 'js', 'html']);
+gulp.task('fonts', function() {
+  gulp.src('./src/assets/fonts/*')
+    .pipe(gulp.dest('./dist/assets/fonts'))
+    .pipe(livereload());
+});
+
+gulp.task('build', ['fonts', 'sass', 'js', 'html'])
+
+gulp.task('watch', function() {
+  livereload.listen();  // Actually start the LiveReload listener.
+  gulp.watch('./src/assets/styles/**/*.scss', ['sass']);
+});
+
+gulp.task('server', function() {
+  if (node) {
+    node.kill();
+  }
+
+  node = spawn('node', ['index.js'], {stdio: 'inherit'});
+  node.on('close', function(code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  })
+
+  livereload.reload();
+});
+
+gulp.task('develop', ['watch', 'server'], function() {
+  gulp.watch(['./src/assets/styles/**/*.scss', './src/**/*.html'], ['build']);
+});
+
+gulp.task('default', ['build']);
+
+process.on('exit', function() {
+    if (node) node.kill()
+})
--- index.js
+++ index.js
@@ -3,7 +3,5 @@
 
 var express = require('express');
 var app = express();
-
 app.use(express.static('dist'));
-
 app.listen(8080);
