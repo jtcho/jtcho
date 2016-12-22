026c13869256a0906fdc45c5afeb616846571bbc
BuildTools <unconfigured@null.spigotmc.org>
2016-12-22 04:52:40 -0500

    Fixed issue in production where incorrect dist folder was being used by forever

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // Main site entry point.
 // Serves simple static content.
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('express');
 var app = express();
-app.use(express.static('dist'));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(express.static('dist'));
+} else if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(process.env.STATIC_DIR));
+}
+
 app.get('/about', function(req, res) {
     res.sendFile(__dirname + '/dist/about.html');
 });
